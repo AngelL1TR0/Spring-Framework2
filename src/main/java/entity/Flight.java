ff523d15ede361b212f90d2923555cc6faab5571
AngelClase <angel.torijasanchezj@iesfm.onmicrosoft.com>
2022-10-26 10:51:04 +0200

    first commit

---
+++ src/main/java/entity/Flight.java
@@ -0,0 +1,12 @@
+package entity;
+
+import java.util.List;
+
+public class Flight {
+
+    private String origin;
+    private String destiny;
+    private String date;
+    private List<String> passagers;
+
+}
