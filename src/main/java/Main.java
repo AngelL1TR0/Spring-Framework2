8a76a64b59ca3fb225e64f1b869a2545c2b3c88b
Angel04 <angeltorija4@gmail.com>
2022-11-02 23:42:56 +0100

    fifth commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
-
     public static void main(String[] args) {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(AirlineConfiguration.class);
         AirlineProgram program = context.getBean(AirlineProgram.class);
         program.execute();
-
     }
 }
